23379b417c0012ab8d7816e28417e1ad0894710b
nexphernandez <nicolaseh3b1112@gmail.com>
2025-09-26 15:45:29 +0100

    Se realiza el ejercicio8

---
+++ Acceso a Datos: "Ficheros"/ejercicios/ejercicio8.php
@@ -0,0 +1,11 @@
+<?php
+    $ruta= "ficheros.tabla5.txt";
+    $file = fopen($ruta, "w+");
+    $multiplicacion = 0;
+    for ($i=1; $i <=10; $i++) { 
+        $multiplicacion = 5 * $i;
+        fwrite($file, "5 x $i = $multiplicacion \n");
+    }
+    fclose($file);
+    echo file_get_contents(filename: $ruta);
+?>

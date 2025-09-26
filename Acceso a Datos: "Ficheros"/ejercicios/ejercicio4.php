79e9cc125fa8891d03bf0f1accce82354f10fbaa
nexphernandez <nicolaseh3b1112@gmail.com>
2025-09-26 15:03:21 +0100

    Se realiza el ejercicio4

--- Acceso a Datos: "Ficheros"/ejercicios/ejercicio4.php
+++ Acceso a Datos: "Ficheros"/ejercicios/ejercicio4.php
@@ -1,6 +1,13 @@
 <?php
-    $nombre = [];
+    $nombres = ["Nico", "Carlos", "David"];
     $ruta = "ficheros/nombres.txt";
     $file = fopen($ruta, "r+");
-    
+    foreach ($nombres as $nombre) {
+        fwrite($file, $nombre . "\n");
+    }
+    rewind($file);
+    while(($linea = fgets($file)) !== false){
+        echo $linea;
+    }
+    fclose($file);
 ?>
